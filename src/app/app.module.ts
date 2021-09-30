139999acb5346e0f1999ad718fc29d614ae3a1fe
RaeKimie <nr.kim91@gmail.com>
2021-09-30 02:01:49 +0100

    created component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MessageComponent } from './message/message.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { LiveWeddingComponent } from './live-wedding/live-wedding.component';
+import { MapComponent } from './map/map.component';
+import { BankComponent } from './bank/bank.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MessageComponent,
+    ContactsComponent,
+    GalleryComponent,
+    LiveWeddingComponent,
+    MapComponent,
+    BankComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/live-wedding/live-wedding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-live-wedding',
+  templateUrl: './live-wedding.component.html',
+  styleUrls: ['./live-wedding.component.scss']
+})
+export class LiveWeddingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
