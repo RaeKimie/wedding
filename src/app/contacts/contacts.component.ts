23b8249eae9b0ba7a9993f913312565521afc678
RaeKimie <nr.kim91@gmail.com>
2021-10-06 19:43:17 +0100

    add contact to bride groom family

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
-  public mainContacts = [{name: "신랑에게 연락하기", iconColor: "blue", tel:'01044598016'}, {name: "신부에게 연락하기", iconColor: "pink", tel:'01089080062'}]
-
+  public mainContacts = [{name: "신랑에게 연락하기", iconColor: "blue", tel:'01044598015'}, {name: "신부에게 연락하기", iconColor: "pink", tel:'01089080062'}]
+  public brideContacts = [{name: "아버지 심기출", iconColor: "blue", tel:'01002110062'}, {name: "어머니 김혜숙", iconColor: "pink", tel:'01049450662'}]
+  public groomContacts = [{name: "아버지 김기황", iconColor: "blue", tel:'01066768016'}, {name: "어머니 김영자", iconColor: "pink", tel:'01022289334'}]
   constructor() { }
 
   ngOnInit(): void {
