2e93301b7c3aabfddd92b3c08abd69acb303da38
RaeKimie <nr.kim91@gmail.com>
2021-11-02 22:19:28 +0000

    add bank expand section

--- src/app/bank/bank.component.ts
+++ src/app/bank/bank.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BankComponent implements OnInit {
 
+  public groomBankDetails = [{name: "홍길동", bank: "국민은행", number: "123-4567-8910"}, {name: "김동수", bank: "국민은행", number: "123-4567-8910"}];
+  public brideBankDetails = [{name: "test", bank: "국민은행", number: "123-4567-8910"}, {name: "test", bank: "국민은행", number: "123-4567-8910"}];
+
+  public groomExpanded = false;
+  public brideExpanded = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public displayGroomBankDetails() {
+    this.groomExpanded = !this.groomExpanded;
+  }
+
+  public displayBrideBankDetails() {
+    this.brideExpanded = !this.brideExpanded;
   }
 
 }
