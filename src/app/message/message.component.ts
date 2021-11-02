ba711306c96bc7af44d447e67a102ca5ce00e147
RaeKimie <nr.kim91@gmail.com>
2021-11-02 23:34:25 +0000

    add heading test example

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.scss']
 })
 export class MessageComponent implements OnInit {
-
+  public words = ['이','제','서','로', ', ', '평','생', '함', '께'];
   constructor() { }
 
   ngOnInit(): void {
