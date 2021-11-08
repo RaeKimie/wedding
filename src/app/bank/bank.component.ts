6628c88ba08fecdd3447e50eef53f3d4573819f0
RaeKimie <nr.kim91@gmail.com>
2021-11-08 16:26:51 +0000

    update bank details

--- src/app/bank/bank.component.ts
+++ src/app/bank/bank.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BankComponent implements OnInit {
 
-  public groomBankDetails = [{name: "홍길동", bank: "국민은행", number: "123-4567-8910"}, {name: "김동수", bank: "국민은행", number: "123-4567-8910"}];
-  public brideBankDetails = [{name: "test", bank: "국민은행", number: "123-4567-8910"}, {name: "test", bank: "국민은행", number: "123-4567-8910"}];
+  public groomBankDetails = [{name: "김황호", bank: "우리은행", number: "1002-234-660543"}, {name: "김기황", bank: "농협은행", number: "146-12-028354"}];
+  public brideBankDetails = [{name: "심아람", bank: "SC제일은행", number: "102-20-250764"}];
 
   public groomExpanded = false;
   public brideExpanded = false;
